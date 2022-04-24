653f03ee9c8cc283e52a3026257ad769cea931e2
steffds <sdesmedt7@gmail.com>
2022-04-24 12:41:05 +0200

    PHP file added

---
+++ skill-projects/php-forms/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <!--Tab Title Info-->
+    <title>Submitting a Form with PHP</title>
+    <!--Meta Data-->
+    <meta charset="utf-8">
+    <meta name="description" content="PHP Form Submission">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="Portfolio, Coding, Web Design, HTML, CSS, PHP">
+    <meta name="author" content="Stephanie De Smedt">
+    <!--Links to CSS stylesheets, fonts, icons etc-->
+    <link rel="stylesheet" href="style.css">
+    </head>
+
+<body>
+    <h1><?php echo "Contact Form"?></h1>
+</body>
+
+</html>
